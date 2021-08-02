e08a2fe2730505ec15eab5917b9013cfee313844
himself65 <himself65@outlook.com>
2021-08-02 15:48:36 +0800

    fix: rename

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 export default function About() {
   useEffect(() => {
     // eslint-disable-next-line no-undef
-    console.log(globalThis.maskwebauthn)
+    console.log(globalThis.masklogin)
   }, [])
   return (
     <>

4c8b94b1f78e92e166bd8cc0de93d1c4af122fb8
himself65 <himself65@outlook.com>
2021-08-17 21:07:39 -0500

    feat: support typescript

--- lib/utils/kebabCase.ts
+++ lib/utils/kebabCase.ts
@@ -1,5 +1,4 @@
-const kebabCase = (str) =>
-  str &&
+const kebabCase = (str: string): string =>
   str
     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
     .map((x) => x.toLowerCase())
